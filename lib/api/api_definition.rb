bdb03ffa69d72d75fd3816c6e6e6d90fe3010626
Tjad I Clark <tjataz@gmail.com>
2015-02-20 02:10:47 +0200

    Change api-definition Time to DateTime

--- lib/api/api_definition.rb
+++ lib/api/api_definition.rb
@@ -63,7 +63,7 @@ module Korwe
       PRIMITIVE_TYPES = {'Object'=>PrimitiveTypeDefinition.new('object',nil), 'Integer' => PrimitiveTypeDefinition.new('int', Integer), 'String'=> PrimitiveTypeDefinition.new('string', String),
                          'Long'=>PrimitiveTypeDefinition.new('long', Integer), 'Float'=>PrimitiveTypeDefinition.new('float', Float), 'Boolean'=>TypeDefinition.new('boolean'),
                          'TrueClass'=>TypeDefinition.new('boolean'), 'FalseClass'=>TypeDefinition.new('boolean'),
-                         'Double'=>PrimitiveTypeDefinition.new('double', Float), 'Time'=>PrimitiveTypeDefinition.new('DateTime', Time)}
+                         'Double'=>PrimitiveTypeDefinition.new('double', Float), 'DateTime'=>PrimitiveTypeDefinition.new('DateTime', Time)}
       PRIMITIVE_TYPES['Object'].inherited=true
       BASIC_GENERIC_TYPES = {'Map'=>GenericTypeDefinition.new('map'), 'List'=>GenericTypeDefinition.new('list'), 'Set'=>GenericTypeDefinition.new('set')}
 
